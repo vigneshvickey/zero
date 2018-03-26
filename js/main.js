59dd7cd5d6bf5c4ecf2e7be660f4206b233100a3
vignesh <vignesh@dexeldesigns.com>
2018-03-26 16:19:15 +0530

    chnages

--- js/main.js
+++ js/main.js
@@ -4,3 +4,57 @@ $(function () {
 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
 });
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
+});
+// Select all links with hashes
+$('.nav li a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+    $('#media').carousel({
+    pause: true,
+    interval: false,
+  });
+
+    var video = document.querySelector('video');       
+video.addEventListener('ended', function() {
+  video.load();     
+});
